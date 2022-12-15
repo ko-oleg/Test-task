27a505d2320afce6301ec379acc01bb615f1f67c
Oleg Kogay <olegatorkogay@gmail.com>
2022-12-15 11:58:28 +0600

    start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_task_for_Akvelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
